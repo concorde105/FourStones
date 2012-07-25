503d9adb04ffc9d8b9bf839e1d1fc556a0d8f19a
Ian Pearson <concorde@main>
2012-07-24 23:41:16 -0500

    further progress
    Added game class. preparing for writing said class

---
+++ src/fourStones/Game.java
@@ -0,0 +1,21 @@
+/*
+ * the game class for Four Stones
+ * shuffles board, runs game
+ */
+
+package fourStones;
+
+
+
+public class Game {
+	
+	public String[][] board = new String[4][10];
+	
+	public void shuffleBoard() {
+		
+		for ( int counter = 0; counter <= 10; counter++ ) {
+			
+		}
+	}
+
+}
--- src/fourStones/Main.java
+++ src/fourStones/Main.java
@@ -1,3 +1,9 @@
+/* 
+ * the main class for Four Stones
+ * provides menu, calls board shuffling and game start classes
+ */
+
+
 package fourStones;
 
 public class Main {
@@ -6,11 +12,22 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		
+		Game game = new Game();
+		
+		
 		System.out.println("Welcome to Four Stones!");
 		System.out.println("What would you like to do?");
 		System.out.printf("[S]tart a new game,\n" +
 				"[R]ead the rules,\n" +
 				"or [c]lose the game?\n\n");
+		
+		String input = "s";
+		if( input == "s" ) {
+			
+		}
+		
+		
 
 	}
 
