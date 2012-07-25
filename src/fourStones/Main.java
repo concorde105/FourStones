4cd116162bc8cee6be6681116d3974d8d0495214
Ian Pearson <concorde@main>
2012-07-24 23:19:30 -0500

    Initial work

---
+++ src/fourStones/Main.java
@@ -0,0 +1,17 @@
+package fourStones;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Welcome to Four Stones!");
+		System.out.println("What would you like to do?");
+		System.out.printf("[S]tart a new game,\n" +
+				"[R]ead the rules,\n" +
+				"or [c]lose the game?\n\n");
+
+	}
+
+}
